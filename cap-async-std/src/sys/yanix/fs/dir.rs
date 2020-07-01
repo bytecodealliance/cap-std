80179508b1940ffd05a047146a8d38415194608c
Dan Gohman <sunfish@mozilla.com>
2020-07-01 08:06:32 -0700

    Adjust mode types to match Rust traits.

--- cap-async-std/src/sys/yanix/fs/dir.rs
+++ cap-async-std/src/sys/yanix/fs/dir.rs
@@ -9,7 +9,10 @@ use async_std::{
         io::{AsRawFd, FromRawFd, IntoRawFd},
     },
 };
-use std::path::{Path, PathBuf};
+use std::{
+    convert::TryInto,
+    path::{Path, PathBuf},
+};
 use yanix::file::{linkat, mkdirat, openat, unlinkat, AtFlag, Mode, OFlag};
 
 pub(crate) struct Dir {
