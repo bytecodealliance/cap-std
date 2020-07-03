d95f66664b0b662af61c31531e65ea8b45eca716
Dan Gohman <sunfish@mozilla.com>
2020-07-03 07:25:41 -0700

    "Inline" cap-primitive's `yanix::common` module.
    Rename cap-primitives/src/yanix/common/fs/* to cap-primitives/src/yanix/fs/*,
    making the module structure simpler.

--- cap-primitives/src/yanix/fs/mod.rs
+++ cap-primitives/src/yanix/fs/mod.rs
@@ -14,6 +14,21 @@ mod stat_unchecked;
 mod symlink_unchecked;
 mod unlink_unchecked;
 
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        pub(crate) use crate::yanix::linux::fs::*;
+    } else {
+        pub(crate) use crate::fs::open_manually_wrapper as open_impl;
+    }
+}
+
+pub(crate) use crate::fs::{
+    canonicalize_manually as canonicalize_impl, link_via_parent as link_impl,
+    mkdir_via_parent as mkdir_impl, readlink_via_parent as readlink_impl,
+    rename_via_parent as rename_impl, stat_via_parent as stat_impl,
+    symlink_via_parent as symlink_impl, unlink_via_parent as unlink_impl,
+};
+
 pub(crate) mod errors;
 
 pub(crate) use file_type_ext::*;
--- cap-primitives/src/yanix/mod.rs
+++ cap-primitives/src/yanix/mod.rs
@@ -1,24 +1,4 @@
-mod common;
 #[cfg(target_os = "linux")]
 mod linux;
 
-pub(crate) mod fs {
-    use cfg_if::cfg_if;
-
-    pub(crate) use super::common::fs::*;
-
-    cfg_if! {
-        if #[cfg(target_os = "linux")] {
-            pub(crate) use super::linux::fs::*;
-        } else {
-            pub(crate) use crate::fs::open_manually_wrapper as open_impl;
-        }
-    }
-
-    pub(crate) use crate::fs::{
-        canonicalize_manually as canonicalize_impl, link_via_parent as link_impl,
-        mkdir_via_parent as mkdir_impl, readlink_via_parent as readlink_impl,
-        rename_via_parent as rename_impl, stat_via_parent as stat_impl,
-        symlink_via_parent as symlink_impl, unlink_via_parent as unlink_impl,
-    };
-}
+pub(crate) mod fs;
