e0618f65e98b780c4c405f7f1a9b69f68e0d4fa4
Jakub Konka <kubkon@jakubkonka.com>
2020-07-21 09:18:55 +0200

    Fix CI after deps upgrade

--- cap-primitives/src/yanix/fs/rmdir_unchecked.rs
+++ cap-primitives/src/yanix/fs/rmdir_unchecked.rs
@@ -1,7 +1,7 @@
 use std::{fs, io, os::unix::io::AsRawFd, path::Path};
-use yanix::file::{unlinkat, AtFlag};
+use yanix::file::{unlinkat, AtFlags};
 
 /// *Unsandboxed* function similar to `rmdir`, but which does not perform sandboxing.
 pub(crate) fn rmdir_unchecked(start: &fs::File, path: &Path) -> io::Result<()> {
-    unsafe { unlinkat(start.as_raw_fd(), path, AtFlag::REMOVEDIR) }
+    unsafe { unlinkat(start.as_raw_fd(), path, AtFlags::REMOVEDIR) }
 }
