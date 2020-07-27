21b8fc02e0145b2209ca505a95d8dc2f52a53327
Dan Gohman <sunfish@mozilla.com>
2020-07-27 16:28:20 -0700

    Add a simple key-value CLI example for `cap-std`.

---
+++ examples/kv-cli.rs
@@ -0,0 +1,56 @@
+use cap_directories::ProjectDirs;
+use std::{env::args, path::PathBuf, str};
+
+fn main() -> anyhow::Result<()> {
+    // Parse command-line arguments.
+    let mut args = args();
+    args.next(); // skip the program name
+    let key: PathBuf = args.next().ok_or_else(usage)?.into();
+    let value = args.next();
+    if args.next().is_some() {
+        return Err(usage());
+    }
+
+    // Obtain the `data_dir` for this program.
+    let project_dirs = ProjectDirs::from(
+        "com.example",
+        "Example Organization",
+        "Cap-std Key-Value CLI Example",
+    )
+    .ok_or_else(no_project_dirs)?;
+    let mut data_dir = project_dirs.data_dir()?;
+
+    if let Some(value) = value {
+        // `kv-cli key value` -- set the value of `key` to `value`
+
+        // If the key contains separators, create the directory.
+        let parent = key.parent();
+        let file_name = key.file_name().ok_or_else(need_filename)?;
+        if let Some(parent) = parent {
+            if !parent.as_os_str().is_empty() {
+                data_dir.create_dir_all(parent)?;
+                data_dir = data_dir.open_dir(parent)?;
+            }
+        }
+
+        // Write the value.
+        data_dir.write(file_name, value)?;
+    } else {
+        // `kv-cli key` -- get the value of `key` and print it.
+        println!("{}", str::from_utf8(&data_dir.read(key)?)?);
+    }
+
+    Ok(())
+}
+
+fn usage() -> anyhow::Error {
+    anyhow::Error::msg("usage: kv-cli <key> [<value>]")
+}
+
+fn need_filename() -> anyhow::Error {
+    anyhow::Error::msg("kv-cli key must end with a filename component (and not `..`)")
+}
+
+fn no_project_dirs() -> anyhow::Error {
+    anyhow::Error::msg("kv-cli requires a home directory")
+}
