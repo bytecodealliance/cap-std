9f83ff83e03924a3eafcef5844d4f540291ff853
Dan Gohman <sunfish@mozilla.com>
2020-07-28 10:01:45 -0700

    Fix trailing whitespace.

--- tests/sys_common/io.rs
+++ tests/sys_common/io.rs
@@ -5,5 +5,5 @@ pub use cap_tempfile::TempDir;
 pub fn tmpdir() -> TempDir {
     // It's ok to wrap this in an unsafe block, rather than an unsafe function,
     // because this function is only used by tests.
-    unsafe { tempdir() }.expect("expected to be able to create a temporary directory") 
+    unsafe { tempdir() }.expect("expected to be able to create a temporary directory")
 }
