9cd47b2c2a8f4eac85720b8ff66113a6dcb88d2e
Dan Gohman <sunfish@mozilla.com>
2020-07-30 10:17:39 -0700

    rustfmt

--- cap-directories/src/lib.rs
+++ cap-directories/src/lib.rs
@@ -1,8 +1,12 @@
 //! Capability-oriented standard directories.
 
 #![deny(missing_docs)]
-#![doc(html_logo_url = "https://raw.githubusercontent.com/sunfishcode/cap-std/main/media/cap-std.svg")]
-#![doc(html_favicon_url = "https://raw.githubusercontent.com/sunfishcode/cap-std/main/media/cap-std.ico")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/sunfishcode/cap-std/main/media/cap-std.svg"
+)]
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/sunfishcode/cap-std/main/media/cap-std.ico"
+)]
 
 use std::io;
 
