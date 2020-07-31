0e7886160761e765b73ae40b6b62d814d4f1a230
Dan Gohman <sunfish@mozilla.com>
2020-07-30 17:03:56 -0700

    Check for the file being deleted after we read its path from /proc/self/fd.
    This avoids a race where the file could be deleted between the check and
    the readlink.

--- cap-primitives/src/fs/get_path.rs
+++ cap-primitives/src/fs/get_path.rs
@@ -8,17 +8,19 @@ use std::{fs, path::PathBuf};
 pub(crate) fn get_path(file: &fs::File) -> Option<PathBuf> {
     use std::os::unix::{fs::MetadataExt, io::AsRawFd};
 
+    // Ignore paths that don't start with '/', which are things like
+    // `socket:[3556564]` or similar.
+    let mut p = PathBuf::from("/proc/self/fd");
+    p.push(&file.as_raw_fd().to_string());
+    let path = fs::read_link(p).ok().filter(|path| path.starts_with('/'))?;
+
     // Linux appends the string " (deleted)" when a file is deleted; avoid
     // treating that as the actual name.
     if file.metadata().ok()?.nlink() == 0 {
         return None;
     }
 
-    // Ignore paths that don't start with '/', which are things like
-    // `socket:[3556564]` or similar.
-    let mut p = PathBuf::from("/proc/self/fd");
-    p.push(&file.as_raw_fd().to_string());
-    fs::read_link(p).ok().filter(|path| path.starts_with('/'))
+    Some(path)
 }
 
 #[cfg(target_os = "macos")]
