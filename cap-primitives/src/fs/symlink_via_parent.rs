26d94399e419f10b135755235bb886450586151a
Dan Gohman <sunfish@mozilla.com>
2020-08-07 07:05:30 -0700

    Implement symlink_file_via_parent etc. for Windows.

--- cap-primitives/src/fs/symlink_via_parent.rs
+++ cap-primitives/src/fs/symlink_via_parent.rs
@@ -30,7 +30,12 @@ pub(crate) fn symlink_file_via_parent(
     new_start: &fs::File,
     new_path: &Path,
 ) -> io::Result<()> {
-    todo!("symlink_file_via_parent")
+    use crate::fs::symlink_file_unchecked;
+    let new_start = MaybeOwnedFile::borrowed(new_start);
+
+    let (new_dir, new_basename) = open_parent(new_start, new_path)?;
+
+    symlink_file_unchecked(old_path, &new_dir, new_basename.as_ref())
 }
 
 /// Implement `symlink_dir` by `open`ing up the parent component of the path and then
@@ -41,5 +46,10 @@ pub(crate) fn symlink_dir_via_parent(
     new_start: &fs::File,
     new_path: &Path,
 ) -> io::Result<()> {
-    todo!("symlink_dir_via_parent")
+    use crate::fs::symlink_dir_unchecked;
+    let new_start = MaybeOwnedFile::borrowed(new_start);
+
+    let (new_dir, new_basename) = open_parent(new_start, new_path)?;
+
+    symlink_dir_unchecked(old_path, &new_dir, new_basename.as_ref())
 }
