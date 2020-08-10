288f932edd82f3161eb75a78cdaa1f8ef66d4db0
Dan Gohman <sunfish@mozilla.com>
2020-08-10 09:02:31 -0700

    Implement `FileTypeExt::is_symlink` for Windows with stable Rust.

--- cap-primitives/src/winx/fs/file_type_ext.rs
+++ cap-primitives/src/winx/fs/file_type_ext.rs
@@ -2,12 +2,19 @@ use std::fs;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub(crate) enum FileTypeExt {
+    #[cfg(feature = "windows_file_type_ext")]
     SymlinkFile,
+
+    #[cfg(feature = "windows_file_type_ext")]
     SymlinkDir,
+
+    #[cfg(not(feature = "windows_file_type_ext"))]
+    SymlinkUnknown,
 }
 
 impl FileTypeExt {
     /// Constructs a new instance of `Self` from the given `std::fs::FileType`.
+    #[cfg(feature = "windows_file_type_ext")]
     #[inline]
     pub(crate) fn from_std(std: fs::FileType) -> Option<Self> {
         use std::os::windows::fs::FileTypeExt;
@@ -20,13 +27,25 @@ impl FileTypeExt {
         })
     }
 
+    #[cfg(not(feature = "windows_file_type_ext"))]
+    #[inline]
+    pub(crate) fn from_std(std: fs::FileType) -> Option<Self> {
+        Some(if std.is_symlink() {
+            Self::SymlinkUnknown
+        } else {
+            return None;
+        })
+    }
+
     /// Creates a `FileType` for which `is_symlink_file()` returns `true`.
+    #[cfg(feature = "windows_file_type_ext")]
     #[inline]
     pub(crate) const fn symlink_file() -> Self {
         Self::SymlinkFile
     }
 
     /// Creates a `FileType` for which `is_symlink_dir()` returns `true`.
+    #[cfg(feature = "windows_file_type_ext")]
     #[inline]
     pub(crate) const fn symlink_dir() -> Self {
         Self::SymlinkDir
