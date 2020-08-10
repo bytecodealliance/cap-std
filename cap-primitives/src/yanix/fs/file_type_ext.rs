bda193cf44016052d3c5490d162e1411b22f2012
Dan Gohman <sunfish@mozilla.com>
2020-08-10 07:59:44 -0700

    Simplify `FileTypeExt` types.

--- cap-primitives/src/yanix/fs/file_type_ext.rs
+++ cap-primitives/src/yanix/fs/file_type_ext.rs
@@ -2,7 +2,7 @@ use crate::fs::FileType;
 use std::fs;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-enum Inner {
+pub(crate) enum FileTypeExt {
     Symlink,
     BlockDevice,
     CharDevice,
@@ -10,27 +10,24 @@ enum Inner {
     Socket,
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-pub(crate) struct FileTypeExt(Inner);
-
 impl FileTypeExt {
     /// Constructs a new instance of `Self` from the given `std::fs::FileType`.
     #[inline]
     pub(crate) fn from_std(std: fs::FileType) -> Option<Self> {
         use std::os::unix::fs::FileTypeExt;
-        Some(Self(if std.is_symlink() {
-            Inner::Symlink
+        Some(if std.is_symlink() {
+            Self::Symlink
         } else if std.is_block_device() {
-            Inner::BlockDevice
+            Self::BlockDevice
         } else if std.is_char_device() {
-            Inner::CharDevice
+            Self::CharDevice
         } else if std.is_fifo() {
-            Inner::Fifo
+            Self::Fifo
         } else if std.is_socket() {
-            Inner::Socket
+            Self::Socket
         } else {
             return None;
-        }))
+        })
     }
 
     /// Constructs a new instance of `FileType` from the given `libc::mode_t`.
@@ -51,35 +48,35 @@ impl FileTypeExt {
     /// Creates a `FileType` for which `is_symlink()` returns `true`.
     #[inline]
     pub(crate) const fn symlink() -> Self {
-        Self(Inner::Symlink)
+        Self::Symlink
     }
 
     /// Creates a `FileType` for which `is_block_device()` returns `true`.
     #[inline]
     pub(crate) const fn block_device() -> Self {
-        Self(Inner::BlockDevice)
+        Self::BlockDevice
     }
 
     /// Creates a `FileType` for which `is_char_device()` returns `true`.
     #[inline]
     pub(crate) const fn char_device() -> Self {
-        Self(Inner::CharDevice)
+        Self::CharDevice
     }
 
     /// Creates a `FileType` for which `is_fifo()` returns `true`.
     #[inline]
     pub(crate) const fn fifo() -> Self {
-        Self(Inner::Fifo)
+        Self::Fifo
     }
 
     /// Creates a `FileType` for which `is_socket()` returns `true`.
     #[inline]
     pub(crate) const fn socket() -> Self {
-        Self(Inner::Socket)
+        Self::Socket
     }
 
     #[inline]
     pub(crate) fn is_symlink(&self) -> bool {
-        self.0 == Inner::Symlink
+        *self == Self::Symlink
     }
 }
