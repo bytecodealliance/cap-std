6af88c548036f5e620784a1e5bae7e01239cace6
Dan Gohman <dev@sunfishcode.online>
2020-08-17 07:30:12 -0700

    Remove a redundant `is_absolute` check.

--- cap-primitives/src/winx/fs/readlink_unchecked.rs
+++ cap-primitives/src/winx/fs/readlink_unchecked.rs
@@ -6,9 +6,6 @@ use std::{
 
 /// *Unsandboxed* function similar to `readlink`, but which does not perform sandboxing.
 pub(crate) fn readlink_unchecked(start: &fs::File, path: &Path) -> io::Result<PathBuf> {
-    if path.is_absolute() {
-        return fs::read_link(path);
-    }
     let full_path = concatenate_or_return_absolute(start, path)?;
     fs::read_link(full_path)
 }
