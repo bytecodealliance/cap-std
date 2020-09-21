6e703d37828d25fc4ceedb842df16b784e63c9ea
Dan Gohman <dev@sunfishcode.online>
2020-09-21 15:59:28 -0700

    Document why Unix-domain socket support is not yet implemented.

--- cap-std/src/os/unix/net/mod.rs
+++ cap-std/src/os/unix/net/mod.rs
@@ -2,7 +2,12 @@
 //!
 //! This corresponds to [`std::os::unix::net`].
 //!
+//! This module is not yet implemented. And it's not easily implementable
+//! on many platforms. See [this POSIX discussion] which ultimately didn't
+//! succeed in adding support to POSIX.
+//!
 //! [`std::os::unix::net`]: https://doc.rust-lang.org/std/os/unix/net/
+//! [this POSIX discussion]: https://www.austingroupbugs.net/view.php?id=980
 
 mod incoming;
 mod unix_datagram;
