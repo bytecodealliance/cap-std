c374981ed916fedfa4a4b5dfa68809b8a7347e33
Dan Gohman <dev@sunfishcode.online>
2021-01-21 07:52:18 -0800

    Add an `IsReadWrite` trait to expose `cap_primitives::fs::is_read_write`.

--- cap-primitives/src/windows/fs/is_read_write_impl.rs
+++ cap-primitives/src/windows/fs/is_read_write_impl.rs
@@ -4,6 +4,7 @@ pub(crate) fn is_read_write_impl(file: &fs::File) -> io::Result<(bool, bool)> {
     let handle = file.as_raw_handle();
     let access_mode = winx::file::query_access_information(handle)?;
     let read = access_mode.contains(winx::file::AccessMode::FILE_READ_DATA);
-    let write = access_mode.contains(winx::file::AccessMode::FILE_WRITE_DATA);
+    let write = access_mode.contains(winx::file::AccessMode::FILE_WRITE_DATA)
+        || access_mode.contains(winx::file::AccessMode::FILE_APPEND_DATA);
     Ok((read, write))
 }
