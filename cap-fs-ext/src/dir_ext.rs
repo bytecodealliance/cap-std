4d27d2f398143fa346457af727d3aa1ecd36192f
Dan Gohman <dev@sunfishcode.online>
2021-02-16 13:25:21 -0800

    Handle symlinks being disabled in remove_file_or_symlink_dir tests.
    In Windows, if "Developer Mode" is not enabled, symlinks cannot be
    created. Disable the symlink tests in those cases.

--- cap-fs-ext/src/dir_ext.rs
+++ cap-fs-ext/src/dir_ext.rs
@@ -298,57 +298,6 @@ impl DirExt for cap_std::fs::Dir {
     }
 }
 
-#[cfg(all(test, feature = "std"))]
-mod std_remove_file_or_symlink_tests {
-    use super::DirExt;
-    use cap_tempfile::TempDir;
-    #[test]
-    fn remove_file() {
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let file = tempdir.create("file").expect("create file to delete");
-        drop(file);
-        tempdir.remove_file_or_symlink("file").expect("delete file");
-        assert!(!tempdir.exists("file"), "deletion worked");
-    }
-    #[test]
-    fn remove_symlink_to_file() {
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let target = tempdir.create("target").expect("create target file");
-        drop(target);
-        tempdir.symlink("target", "link").expect("create symlink");
-        assert!(tempdir.exists("link"), "link exists");
-        tempdir
-            .remove_file_or_symlink("link")
-            .expect("delete symlink");
-        assert!(!tempdir.exists("link"), "link deleted");
-        assert!(tempdir.exists("target"), "target not deleted");
-    }
-    #[test]
-    fn remove_symlink_to_dir() {
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let target = tempdir.create_dir("target").expect("create target dir");
-        drop(target);
-        tempdir.symlink("target", "link").expect("create symlink");
-        assert!(tempdir.exists("link"), "link exists");
-        tempdir
-            .remove_file_or_symlink("link")
-            .expect("delete symlink");
-        assert!(!tempdir.exists("link"), "link deleted");
-        assert!(tempdir.exists("target"), "target not deleted");
-    }
-    #[test]
-    fn do_not_remove_dir() {
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let subdir = tempdir.create_dir("subdir").expect("create dir");
-        drop(subdir);
-        assert!(tempdir.exists("subdir"), "subdir created");
-        tempdir
-            .remove_file_or_symlink("subdir")
-            .expect_err("should not delete empty directory");
-        assert!(tempdir.exists("subdir"), "subdir not deleted");
-    }
-}
-
 #[cfg(feature = "async_std")]
 impl DirExt for cap_async_std::fs::Dir {
     #[inline]
--- tests/dir-ext.rs
+++ tests/dir-ext.rs
@@ -1,61 +1,63 @@
+mod sys_common;
+
 use cap_fs_ext::DirExt;
 use cap_tempfile::TempDir;
 
-use sys_common::{io::tmpdir, symlink_supported};
+use sys_common::symlink_supported;
 
-    #[test]
-    fn remove_file() {
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let file = tempdir.create("file").expect("create file to delete");
-        drop(file);
-        tempdir.remove_file_or_symlink("file").expect("delete file");
-        assert!(!tempdir.exists("file"), "deletion worked");
+#[test]
+fn remove_file() {
+    let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
+    let file = tempdir.create("file").expect("create file to delete");
+    drop(file);
+    tempdir.remove_file_or_symlink("file").expect("delete file");
+    assert!(!tempdir.exists("file"), "deletion worked");
+}
+
+#[test]
+fn remove_symlink_to_file() {
+    if !symlink_supported() {
+        return;
     }
 
-    #[test]
-    fn remove_symlink_to_file() {
-        if !symlink_supported() {
-            return;
-        }
+    let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
+    let target = tempdir.create("target").expect("create target file");
+    drop(target);
+    tempdir.symlink("target", "link").expect("create symlink");
+    assert!(tempdir.exists("link"), "link exists");
+    tempdir
+        .remove_file_or_symlink("link")
+        .expect("delete symlink");
+    assert!(!tempdir.exists("link"), "link deleted");
+    assert!(tempdir.exists("target"), "target not deleted");
+}
 
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let target = tempdir.create("target").expect("create target file");
-        drop(target);
-        tempdir.symlink("target", "link").expect("create symlink");
-        assert!(tempdir.exists("link"), "link exists");
-        tempdir
-            .remove_file_or_symlink("link")
-            .expect("delete symlink");
-        assert!(!tempdir.exists("link"), "link deleted");
-        assert!(tempdir.exists("target"), "target not deleted");
+#[test]
+fn remove_symlink_to_dir() {
+    if !symlink_supported() {
+        return;
     }
 
-    #[test]
-    fn remove_symlink_to_dir() {
-        if !symlink_supported() {
-            return;
-        }
+    let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
+    let target = tempdir.create_dir("target").expect("create target dir");
+    drop(target);
+    tempdir.symlink("target", "link").expect("create symlink");
+    assert!(tempdir.exists("link"), "link exists");
+    tempdir
+        .remove_file_or_symlink("link")
+        .expect("delete symlink");
+    assert!(!tempdir.exists("link"), "link deleted");
+    assert!(tempdir.exists("target"), "target not deleted");
+}
 
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let target = tempdir.create_dir("target").expect("create target dir");
-        drop(target);
-        tempdir.symlink("target", "link").expect("create symlink");
-        assert!(tempdir.exists("link"), "link exists");
-        tempdir
-            .remove_file_or_symlink("link")
-            .expect("delete symlink");
-        assert!(!tempdir.exists("link"), "link deleted");
-        assert!(tempdir.exists("target"), "target not deleted");
-    }
-
-    #[test]
-    fn do_not_remove_dir() {
-        let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
-        let subdir = tempdir.create_dir("subdir").expect("create dir");
-        drop(subdir);
-        assert!(tempdir.exists("subdir"), "subdir created");
-        tempdir
-            .remove_file_or_symlink("subdir")
-            .expect_err("should not delete empty directory");
-        assert!(tempdir.exists("subdir"), "subdir not deleted");
-    }
+#[test]
+fn do_not_remove_dir() {
+    let tempdir = unsafe { TempDir::new() }.expect("create tempdir");
+    let subdir = tempdir.create_dir("subdir").expect("create dir");
+    drop(subdir);
+    assert!(tempdir.exists("subdir"), "subdir created");
+    tempdir
+        .remove_file_or_symlink("subdir")
+        .expect_err("should not delete empty directory");
+    assert!(tempdir.exists("subdir"), "subdir not deleted");
+}
