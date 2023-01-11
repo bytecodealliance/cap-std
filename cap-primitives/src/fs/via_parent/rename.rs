3a1af57f7cb9286d12f0cfcbb7cf06c57b78d61f
Dan Gohman <dev@sunfishcode.online>
2023-01-11 12:35:00 -0800

    Only fix trailing slashes on Unix.
    And fix trailing slashes by re-appending a slash.

--- cap-primitives/src/fs/via_parent/rename.rs
+++ cap-primitives/src/fs/via_parent/rename.rs
@@ -1,5 +1,7 @@
 use super::open_parent;
-use crate::fs::{rename_unchecked, strip_dir_suffix, MaybeOwnedFile};
+use crate::fs::{
+    append_dir_suffix, path_has_trailing_slash, rename_unchecked, strip_dir_suffix, MaybeOwnedFile,
+};
 use std::path::Path;
 use std::{fs, io};
 
@@ -17,11 +19,27 @@ pub(crate) fn rename(
     // As a special case, `rename` ignores a trailing slash rather than treating
     // it as equivalent to a trailing slash-dot, so strip any trailing slashes
     // for the purposes of `open_parent`.
-    let old_path_stripped = strip_dir_suffix(old_path);
-    let new_path_stripped = strip_dir_suffix(new_path);
+    //
+    // And on Unix, remember whether the source started with a slash so that we
+    // can still fail if it is and the source is a regular file.
+    #[cfg(unix)]
+    let old_starts_with_slash = path_has_trailing_slash(old_path);
+    let old_path = strip_dir_suffix(old_path);
+    let new_path = strip_dir_suffix(new_path);
 
-    let (old_dir, old_basename) = open_parent(old_start, &*old_path_stripped)?;
-    let (new_dir, new_basename) = open_parent(new_start, &*new_path_stripped)?;
+    let (old_dir, old_basename) = open_parent(old_start, &*old_path)?;
+    let (new_dir, new_basename) = open_parent(new_start, &*new_path)?;
+
+    // On Unix, re-append a slash if needed.
+    #[cfg(unix)]
+    let concat;
+    #[cfg(unix)]
+    let old_basename = if old_starts_with_slash {
+        concat = append_dir_suffix(old_basename.to_owned().into());
+        concat.as_os_str()
+    } else {
+        old_basename
+    };
 
     rename_unchecked(
         &old_dir,
