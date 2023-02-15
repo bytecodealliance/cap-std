e00166883402a2b28e79f00037da8686c2c93df5
Dan Gohman <dev@sunfishcode.online>
2023-02-15 09:31:48 -0800

    Use the `NtCreateFile` path for implementing `stat` on Windows. (#295)
    Always use `open_unchecked` with no access for implementing `stat` on Windows,
    as that now uses the new `NtCreateFile` path.

--- cap-primitives/src/windows/fs/stat_unchecked.rs
+++ cap-primitives/src/windows/fs/stat_unchecked.rs
@@ -1,16 +1,12 @@
-#[cfg(windows_by_handle)]
-use super::get_path::concatenate;
 use crate::fs::{FollowSymlinks, Metadata};
 use std::path::Path;
 use std::{fs, io};
-#[cfg(not(windows_by_handle))]
-use windows_sys::Win32::Storage::FileSystem::{
-    FILE_FLAG_BACKUP_SEMANTICS, FILE_FLAG_OPEN_REPARSE_POINT,
-};
-#[cfg(not(windows_by_handle))]
 use {
     crate::fs::{open_unchecked, OpenOptions},
     std::os::windows::fs::OpenOptionsExt,
+    windows_sys::Win32::Storage::FileSystem::{
+        FILE_FLAG_BACKUP_SEMANTICS, FILE_FLAG_OPEN_REPARSE_POINT,
+    },
 };
 
 /// *Unsandboxed* function similar to `stat`, but which does not perform
@@ -20,35 +16,24 @@ pub(crate) fn stat_unchecked(
     path: &Path,
     follow: FollowSymlinks,
 ) -> io::Result<Metadata> {
-    // When we have `windows_by_handle`, we just call `fs::metadata` etc. and it
-    // has everything.
-    #[cfg(windows_by_handle)]
-    {
-        let full_path = concatenate(start, path)?;
-        match follow {
-            FollowSymlinks::Yes => fs::metadata(full_path),
-            FollowSymlinks::No => fs::symlink_metadata(full_path),
+    // Attempt to open the file to get the metadata that way, as that gives
+    // us all the info.
+    let mut opts = OpenOptions::new();
+
+    // Explicitly request no access, because we're just querying metadata.
+    opts.access_mode(0);
+
+    match follow {
+        FollowSymlinks::Yes => {
+            opts.custom_flags(FILE_FLAG_BACKUP_SEMANTICS);
+            opts.follow(FollowSymlinks::Yes);
         }
-        .map(Metadata::from_just_metadata)
+        FollowSymlinks::No => {
+            opts.custom_flags(FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS);
+            opts.follow(FollowSymlinks::No);
+        }
     }
 
-    // Otherwise, attempt to open the file to get the metadata that way, as
-    // that gives us all the info.
-    #[cfg(not(windows_by_handle))]
-    {
-        let mut opts = OpenOptions::new();
-        opts.access_mode(0);
-        match follow {
-            FollowSymlinks::Yes => {
-                opts.custom_flags(FILE_FLAG_BACKUP_SEMANTICS);
-                opts.follow(FollowSymlinks::Yes);
-            }
-            FollowSymlinks::No => {
-                opts.custom_flags(FILE_FLAG_OPEN_REPARSE_POINT | FILE_FLAG_BACKUP_SEMANTICS);
-                opts.follow(FollowSymlinks::No);
-            }
-        }
-        let file = open_unchecked(start, path, &opts)?;
-        Metadata::from_file(&file)
-    }
+    let file = open_unchecked(start, path, &opts)?;
+    Metadata::from_file(&file)
 }
