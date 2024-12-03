087035f4a9dd43e70600bb780bbeb4187cc48ef1
Dan Gohman <dev@sunfishcode.online>
2024-12-02 16:15:09 -0800

    Strip trailing whitespace too.

--- cap-primitives/src/windows/fs/open_impl.rs
+++ cap-primitives/src/windows/fs/open_impl.rs
@@ -14,7 +14,7 @@ pub(crate) fn open_impl(
     // See: https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
     if let Some(stem) = file_prefix(path) {
         if let Some(stemstr) = stem.to_str() {
-            match stemstr.to_uppercase().as_str() {
+            match stemstr.trim_end().to_uppercase().as_str() {
                 "CON" | "PRN" | "AUX" | "NUL" | "COM0" | "COM1" | "COM2" | "COM3" | "COM4"
                 | "COM5" | "COM6" | "COM7" | "COM8" | "COM9" | "COM¹" | "COM²" | "COM³"
                 | "LPT0" | "LPT1" | "LPT2" | "LPT3" | "LPT4" | "LPT5" | "LPT6" | "LPT7"
