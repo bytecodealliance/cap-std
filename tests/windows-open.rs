3d4542c38c0303acacde26f43938e48525a1c0a5
Dan Gohman <dev@sunfishcode.online>
2024-12-03 16:47:13 -0800

    Correctly reject device files with multi-dot filename extensions. (#378)
    * Correctly reject device files with multi-dot filename extensions.
    
    When rejecting device names such as "CON" and "CON.txt", reject
    filenames with multiple-dot extensions too, such as "CON.txt.gz".
    
    * Strip trailing whitespace too.

--- tests/windows-open.rs
+++ tests/windows-open.rs
@@ -134,30 +134,43 @@ fn windows_open_special() {
     // Opening any of these should fail.
     for device in &[
         "CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7",
-        "COM8", "COM9", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8",
-        "LPT9",
+        "COM8", "COM9", "COM¹", "COM²", "COM³", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
+        "LPT6", "LPT7", "LPT8", "LPT9", "LPT¹", "LPT²", "LPT³",
     ] {
-        tmpdir.open(device).unwrap_err();
-        tmpdir.open(&format!(".\\{}", device)).unwrap_err();
-        tmpdir.open(&format!("{}.ext", device)).unwrap_err();
-        tmpdir.open(&format!(".\\{}.ext", device)).unwrap_err();
+        for suffix in &[
+            "",
+            " ",
+            ".",
+            ". ",
+            ".ext",
+            ".ext.",
+            ".ext. ",
+            ".ext ",
+            ".ext.more",
+            ".ext.more.",
+            ".ext.more ",
+            ".ext.more. ",
+            ".ext.more .",
+        ] {
+            let name = format!("{}{}", device, suffix);
+            eprintln!("testing '{}'", name);
 
-        let mut options = cap_std::fs::OpenOptions::new();
-        options.write(true);
-        tmpdir.open_with(device, &options).unwrap_err();
-        tmpdir
-            .open_with(&format!(".\\{}", device), &options)
-            .unwrap_err();
-        tmpdir
-            .open_with(&format!("{}.ext", device), &options)
-            .unwrap_err();
-        tmpdir
-            .open_with(&format!(".\\{}.ext", device), &options)
-            .unwrap_err();
+            match tmpdir.open(&name).unwrap_err().kind() {
+                std::io::ErrorKind::NotFound | std::io::ErrorKind::PermissionDenied => {}
+                kind => panic!("unexpected error: {:?}", kind),
+            }
 
-        tmpdir.create(device).unwrap_err();
-        tmpdir.create(&format!(".\\{}", device)).unwrap_err();
-        tmpdir.create(&format!("{}.ext", device)).unwrap_err();
-        tmpdir.create(&format!(".\\{}.ext", device)).unwrap_err();
+            let mut options = cap_std::fs::OpenOptions::new();
+            options.write(true);
+            match tmpdir.open_with(&name, &options).unwrap_err().kind() {
+                std::io::ErrorKind::NotFound | std::io::ErrorKind::PermissionDenied => {}
+                kind => panic!("unexpected error: {:?}", kind),
+            }
+
+            match tmpdir.create(&name).unwrap_err().kind() {
+                std::io::ErrorKind::NotFound | std::io::ErrorKind::PermissionDenied => {}
+                kind => panic!("unexpected error: {:?}", kind),
+            }
+        }
     }
 }
